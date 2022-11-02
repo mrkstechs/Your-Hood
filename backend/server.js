62efa53e3f54e11aed19bc342f14c16af06006b5
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-02 12:00:48 +0000

    added based packages and linked static files

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 const path = require('path')
 const fs = require('fs')
 const express = require('express')
+const cors = require('cors')
 const apiRouter = require('./routes/api')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use('/api', apiRouter)
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../frontend/assets')))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome to Your Hood!</h1>')
-})
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/index.html')))
+
+app.use('/api', apiRouter)
 
 app.listen(port,  () => console.log(`> Your Hood online. http://localhost:${port}`))
 
